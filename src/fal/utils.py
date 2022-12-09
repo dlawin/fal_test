08ab48636f88d6b2427fa883d5def3e14a1aac8d
Dan <daniel@datafold.com>
2022-12-09 14:09:10 -0700

    change log statement

--- src/fal/utils.py
+++ src/fal/utils.py
@@ -16,7 +16,7 @@ if TYPE_CHECKING:
 def print_run_info(scripts: List["FalScript"]):
     """Print information on the current fal run."""
     models_str = "\n".join(map(lambda script: script.id, scripts))
-    LOGGER.info(f"Starting fal run for following models and scripts: \n{models_str}\n")
+    LOGGER.info(f"Starting auto-diff for: \n{models_str}\n")
 
 
 class DynamicIndexProvider:
